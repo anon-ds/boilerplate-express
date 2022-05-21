2f277f444847fdd9c97158acf12f4e7dc0bb433c
vehicleLoan <vehicleloan920@gmail.com>
2022-05-21 22:51:38 +0530

    added static css files

--- myApp.js
+++ myApp.js
@@ -1,7 +1,13 @@
 let express = require('express');
+
+let stylesPath = __dirname + "public/style.css";
+const addStyle = express.static(stylesPath);
+
+
 console.log("Hello World");
 let app = express();
 
+app.use(addStyle);
 app.get("/",(req,res)=>{
     let absolutePath = __dirname + "/views/index.html";
     res.sendFile(absolutePath);
